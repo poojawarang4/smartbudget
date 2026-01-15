da13b736bb061a7945e0fdf18b918bcb93c5f2e9
poojawarang4 <poojawarang4>
2026-01-15 14:55:58 +0530

    In Sidebar trend option add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { NoBudget } from './pages/no-budget/no-budget';
 import { Budget } from './pages/budget/budget';
 import { Overview } from './overview/overview';
+import { BudgetTrendComponent } from './budget-trend-component/budget-trend-component';
+import { Signup } from './signup/signup';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'budget', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'no-budget', component: NoBudget },
   { path: 'budget', component: Budget },
-  { path: 'overview', component: Overview }
+  { path: 'overview', component: Overview },
+  { path: 'trend', component: BudgetTrendComponent },
 ];
